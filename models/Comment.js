afcda797bff88ad6cdc16fbf302a1552e50d63e9
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-07-07 19:43:59 -0700

    created Location and Comment schemas

---
+++ models/Comment.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+	text: String,
+	createdAt: {type: Date, default: Date.now },
+	author: {
+		id: {
+			type: Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	}
+});
+mongoose.model("Comment", commentSchema);
--- models/Location.js
+++ models/Location.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const LocationSchema = new Schema({
+const locationSchema = new Schema({
 	name: String,
 	price: String, 
+	image: String,
 	description: String,
-	location: String,
+	locality: String,
+	lat: Number,
+	lng: Number,
+	createdAt: { type: Date, default: Date.now },
+	author: {
+		id: {
+			type: Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	},
+	comments: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
 });
-mongoose.model("Location", LocationSchema);
+mongoose.model("Location", locationSchema);
