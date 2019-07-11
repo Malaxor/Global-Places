1c8125aba854c914fb57f3c55f1203188fafbc55
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-07-11 14:36:39 -0700

    stylistic changes; heroku deployable

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+   mongoURI: process.env.MONGO_URI,
+   geocoderAPI: process.env.GEOCODER_API,
+   cloudinaryAPIkey: process.env.CLOUDINARY_API_KEY,
+   cloudinaryAPIsecret: process.env.CLOUDINARY_API_SECRET
+};
